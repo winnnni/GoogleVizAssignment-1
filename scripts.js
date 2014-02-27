2c5a0803f8e8dd33f47e92d9e904775d71088776
winnnni <jz2521@columbia.edu>
2014-02-27 17:57:48 -0500

    scripts done andtypo fixed

--- scripts.js
+++ scripts.js
@@ -24,34 +24,72 @@
  * 			tell library where i want to draw the data
  * 			add options: headline of my chart 
  */
-console.log ("The First step");
+console.log ("The First step, I am going to create a line chart of unemployment today.");
 
 //USUNEMPLOYMENT is the local name of thejason file i just loaded
 //USUNEMPLOYMENT is the name i picked myself, can be any.
 function dataLoaded(USUNEMPLOYMENT){
 	console.log(USUNEMPLOYMENT);
 	
-	var myOriginalJobData = USUNEMPLOYMENT.observation;
+	var myOriginalJobData = USUNEMPLOYMENT.observations;
+	//(check typos! copy paste file names)
 	//create a new name for my original data set
 	
 	//I'm trying to construct an array of arrays
-	var myRefineData =[];
+	var myRefineDataArray =[];
 	
 	//i need header to be the first array
 	//create a header before the for loop
 	var Header = ["Date","Value"];
-	myRefine.push(Header);
+	myRefineDataArray.push(Header);
+	
+	//first parameter is starting point
+	for (var i=0; i<myOriginalJobData.length; i++){
+		// count begins with 0 
+		
+		//create refrerence to current object in list
+		var DataNeeded = myOriginalJobData[i];
+		
+		//I dont want number to be shown in strings.
+		var RefineArray =[DataNeeded.date,Number(DataNeeded.value)];
+		//I want numbers instead of strings
+		
+		myRefineDataArray.push(RefineArray);
+	}
+	//this is the end the for loop
+	console.log(myRefineDataArray);
+	//console.log the data after the for loop
+	
+	//feed data to visulization library
+	var data = google.visualization.arrayToDataTable(myRefineDataArray);
+	
+	
+	//create options object to actually customeize
+	//I need a headline
+	var options = {
+          title: "Unemployment rate since 1980"
+        };
+
+	//figure out what type of chart i want
+	var myLineChart = new google.visualization.LineChart(document.getElementById("linechartunempDiv"));
+	//copy paste the Div I created in the webpage
+        myLineChart.draw(data,options);
+}
 
 function googleLoaded(){
-	console.log("google loaded");
+	console.log("I can see line chart now.");
 	
-	//
-	$.get("UEMP270V_data.json.",dataLoaded,"json");
+	//use the jQeury get function to load my json file
+	//takes 3 parameter 
+	//first is the name of the file
+	//second is the function to call once the file is loaded, I keep the "dataLoaded"
+	//third is a string of the file type to expect
+	$.get("UEMP270V_data.json",dataLoaded,"json");
 }
 
 function pageLoaded(){
 	
-	console.log("got to page loaded");
+	console.log("I have all preparations done!");
 	
 	// load the google viz library
 	google.load("visualization", "1", {packages:["corechart"], callback:"googleLoaded"});
